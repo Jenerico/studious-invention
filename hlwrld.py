fad24ba11abc87d3440159f4ae474d951bbcd4d2
Jenerico <93575335+Jenerico@users.noreply.github.com>
2023-07-06 20:01:06 +0200

    Update hlwrld.py

--- hlwrld.py
+++ hlwrld.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def hello_world():
+    metrics.counter('hello_world_requests', 'Number of requests to hello_world endpoint').inc()
     return 'Hello, world!'
 
 @app.route('/health')
@@ -12,8 +15,6 @@ def health_check():
 
 @app.route('/ready')
 def ready_check():
-    # Проверяем, готово ли приложение к обработке запросов
-    # В данном примере всегда возвращаем 'OK'
     return 'OK'
 
 if __name__ == '__main__':
