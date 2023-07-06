483b8d3242583074f7cbe9fc97bcd32f01c57d80
Jenerico <93575335+Jenerico@users.noreply.github.com>
2023-07-06 21:40:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask
-from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
-metrics = PrometheusMetrics(app)
+
+hello_counter = Counter("hello_world_counter", "Hello world endpoint hit count")
 
 @app.route('/')
 def hello_world():
-    metrics.counter('hello_world_requests', 'Number of requests to hello_world endpoint').inc()
+    hello_counter.inc()
     return 'Hello, world!'
 
 @app.route('/health')
 def health_check():
-    return 'OK'
+    return 'OK', 200
 
 @app.route('/ready')
 def ready_check():
-    return 'OK'
+    return 'OK', 200
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {CONTENT_TYPE_LATEST: "text/plain"}
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=5000)
