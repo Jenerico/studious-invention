73283cb33167a56c5057cecff77a7d3907fbbca1
Jenerico <93575335+Jenerico@users.noreply.github.com>
2023-07-06 19:52:34 +0200

    Update hlwrld.py

--- hlwrld.py
+++ hlwrld.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, world!'
 
+@app.route('/health')
+def health_check():
+    return 'OK'
+
+@app.route('/ready')
+def ready_check():
+    # Проверяем, готово ли приложение к обработке запросов
+    # В данном примере всегда возвращаем 'OK'
+    return 'OK'
+
 if __name__ == '__main__':
     app.run()
